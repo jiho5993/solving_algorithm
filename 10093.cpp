cff20e5ed9a8ccd5b015071c54f02ad26d23992b
Pjh <jiho5993@naver.com>
2019-07-18 20:00:58 +0900

    숫자

---
+++ 10093.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 2147483648;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    ll a, b;
+    cin >> a >> b;
+    if(a > b) {
+        ll tmp = b;
+        b = a;
+        a = tmp;
+    }
+    a != b ? pt(b-a-1) : pt(0);
+    for(ll i=a+1; i<b; i++) pt1(i);
+
+    return 0;
+}
