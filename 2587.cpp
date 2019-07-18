a953c7c2518a84851d931b2a002a6a499f90b486
Pjh <jiho5993@naver.com>
2019-07-18 20:02:30 +0900

    대표값2

---
+++ 2587.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 2147483648;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int arr[5], sum=0;
+    fr(i, 0, 5) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    pt(sum/5);
+    sort(arr, arr+5);
+    pt(arr[2]);
+
+    return 0;
+}
