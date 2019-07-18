0755046ebd5122f03dd242ddb3676c74976d1a90
Pjh <jiho5993@naver.com>
2019-07-18 20:02:23 +0900

    홀수

---
+++ 2576.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 2147483648;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    bool flag = false;
+    int sum=0, s=101;
+    fr(i, 0, 7) {
+        int a; cin >> a;
+        if(a%2) {
+            if(s > a) s = a;
+            sum += a;
+            flag = true;
+        }
+    }
+    if(flag) {pt(sum); pt(s);}
+    else pt(-1);
+
+    return 0;
+}
