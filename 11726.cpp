37194cdcc049a99fb372cf8b9afe04c7fed8d535
Pjh <jiho5993@naver.com>
2019-07-22 23:16:59 +0900

    2×n 타일링

---
+++ 11726.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 2147483648;
+const int MOD = 1000000007;
+
+int dp[100005];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int n; cin >> n;
+    dp[1] = 1;
+    dp[2] = 2;
+    fr(i, 3, n+1) dp[i] = (dp[i-2]+dp[i-1])%10007; // overflow
+    pt(dp[n]);
+
+    return 0;
+}
