6785687db762f694940d7c48c134df912ae6fe62
Pjh <jiho5993@naver.com>
2019-08-02 23:50:38 +0900

    소인수분해

---
+++ 11653.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int n; cin >> n;
+    for(int i=2; i*i <= n; i++) {
+        while(n%i==0) {
+            pt(i);
+            n /= i;
+        }
+    }
+    if(n != 1) pt(n);
+
+    return 0;
+}
