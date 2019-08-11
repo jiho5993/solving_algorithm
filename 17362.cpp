33a894644f933f363aa70ba08cdf454527828f97
Pjh <jiho5993@naver.com>
2019-08-12 01:41:07 +0900

    수학은 체육과목 입니다 2

---
+++ 17362.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int n; cin >> n;
+    while(n > 5) n -= 8;
+    if(n == 1) pt(1);
+    else if(n == 0 || n == 2) pt(2);
+    else if(n == -1 || n == 3) pt(3);
+    else if(n == -2 || n == 4) pt(4);
+    else pt(5);
+
+    return 0;
+}
