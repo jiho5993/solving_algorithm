440634c317c7dd66a63db86809ab028b1a73f83f
Pjh <jiho5993@naver.com>
2019-08-21 15:36:44 +0900

    공약수

---
+++ 5618.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int arr[3], n, m=INT32_MAX;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    cin >> n;
+    fr(i, 0, n) {
+        cin >> arr[i];
+        m = min(m, arr[i]);
+    }
+    fr(i, 1, m)
+        if(arr[0]%i == 0 && arr[1]%i == 0 && arr[2]%i == 0)
+            pt(i);
+
+    return 0;
+}
