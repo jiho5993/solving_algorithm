6db024e425d2edaf5a508e0e83401a2240ecf74f
Pjh <jiho5993@naver.com>
2019-08-24 01:04:30 +0900

    분수찾기

---
+++ 1193.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int a, n=0; cin >> a;
+    while(a > 0) n++, a-=n;
+    if(!(n%2)) cout << a+n << '/' << 1-a;
+    else cout << 1-a << '/' << a+n;
+
+    return 0;
+}
