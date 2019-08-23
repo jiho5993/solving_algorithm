a3ade2816e361e65b8bb69f976e19e3d24ef2cb0
Pjh <jiho5993@naver.com>
2019-08-24 01:04:22 +0900

    수도요금

---
+++ 10707.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int a, b, c, d, p, rsX, rsY;
+    cin >> a >> b >> c >> d >> p;
+    rsX = a*p, rsY = c >= p ? b : b+(p-c)*d;
+    pt(rsX < rsY ? rsX : rsY);
+
+    return 0;
+}
