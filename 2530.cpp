9edbea507d8122ba604ade2a4c447257d38dae18
Pjh <jiho5993@naver.com>
2019-08-24 01:04:52 +0900

    인공지능 시계

---
+++ 2530.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    c += d;
+    b += c/60, c %= 60;
+    a += b/60, b %= 60;
+    a %= 24;
+    pt2(a, b), pt(c);
+
+    return 0;
+}
