44ae47911b6ce07d0f3196da2e6889c6ca321419
Pjh <jiho5993@naver.com>
2019-08-24 01:05:15 +0900

    저작권

---
+++ 2914.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int a, b; cin >> a >> b;
+    pt(a*(b-1)+1);
+
+    return 0;
+}
