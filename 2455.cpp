94af736d9cbfc3bda55ebbfa409e61ecf03d7aaa
Pjh <jiho5993@naver.com>
2019-08-24 01:04:45 +0900

    지능형 기차

---
+++ 2455.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int rs=0, q=0;
+    for(int i=0; i<4; i++) {
+        int a, b; cin >> a >> b;
+        q += b-a;
+        rs = max(rs, q);
+    }
+    pt(rs);
+
+    return 0;
+}
