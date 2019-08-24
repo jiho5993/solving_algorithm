d4e8af0fb58f581abe68520f1c6696a787aca39a
Pjh <jiho5993@naver.com>
2019-08-25 01:38:10 +0900

    두 수 비교하기

---
+++ 1330.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int a, b; cin >> a >> b;
+    if(a < b) pt("<");
+    else if(a > b) pt(">");
+    else pt("==");
+
+    return 0;
+}
