da9cbbccabd640d2bdca97f4c9539c7432737190
Pjh <jiho5993@naver.com>
2019-08-26 01:55:48 +0900

    Manthan, Codefest 19

---
+++ A.XORinacci.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int t; cin >> t;
+    while(t--) {
+        ll a, b, n, arr[3]; cin >> a >> b >> n;
+        arr[0] = a, arr[1] = b, arr[2] = a^b;
+        pt(arr[n%3]);
+    }
+
+    return 0;
+}
