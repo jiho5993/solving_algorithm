f2a9faf7fe9ab58c821f72927aa58b61f6ee30cb
Pjh <jiho5993@naver.com>
2019-09-01 23:39:57 +0900

    과일 서리

---
+++ 17213.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<int> > vii;
+typedef pair<int, int> pii;
+typedef priority_queue<int> pqi;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define pt(a) cout << (a) << '\n'
+#define pt1(a) cout << (a) << ' '
+#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int n, m; cin >> n >> m;
+    m -= n;
+    ll a=1, b=1;
+    fr(i, m+1, n+m) a*=i;
+    fr(i, 1, n) b*=i;
+    pt(a/b);
+
+    return 0;
+}
