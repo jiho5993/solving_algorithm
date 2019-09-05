4b6395d2bf19ca18c0e03752574999d6b914a741
Pjh <jiho5993@naver.com>
2019-09-05 18:27:12 +0900

    마이크로소프트 로고

---
+++ 5338.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout << "       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|";
+
+    return 0;
+}
