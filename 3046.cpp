751cb2556429ad6421a894c011df10a2dff17224
Pjh <jiho5993@naver.com>
2019-09-14 00:17:28 +0900

    R2

---
+++ 3046.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a, b; cin >> a >> b;
+    cout << 2*b - a;
+
+    return 0;
+}
