d7065cf53340c5f13b0d759202ceb3445c7c90fb
jiho5993 <jiho5993@naver.com>
2019-09-20 13:01:24 +0900

    운동장 한 바퀴

---
+++ 16486.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI 3.141592
+
+int main() {
+    cin.tie(0);
+
+    int a, b; cin >> a >> b;
+    printf("%.6f", double(2*PI*b + 2*a));
+
+    return 0;
+}
