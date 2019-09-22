63344647a45dc16235a56edf10e48f3cc5db26b4
Pjh <jiho5993@naver.com>
2019-09-23 00:27:26 +0900

    BOJ_9655.돌 게임

---
+++ BOJ/9655.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i, a, b) for(int i=a; i<b; i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    cout << (n%2 ? "SK" : "CY");
+
+    return 0;
+}
