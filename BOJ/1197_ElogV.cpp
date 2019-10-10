2ac0269c1079cd6879708c720439300070b3d35c
박지호 <jhpark@bagjihoui-MacBook-Air.local>
2019-10-10 16:21:10 +0900

    BOJ_1197.최소 스패닝 트리.ElogV

---
+++ BOJ/1197_ElogV.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF INT32_MAX
+typedef pair<int, int> pii;
+
+int V, E;
+vector<pii> adj[10001];
+priority_queue<pii, vector<pii>, greater<pii> > dist;
+bool selected[10001];
+
+int prim(int pn) {
+    int ret = 0;
+    dist.push({0, 1});
+    for(int i=1; i<=pn; i++) {
+        int now=-1, min_dist=INF;
+        while(!dist.empty()) {
+            now = dist.top().second;
+            if(!selected[now]) {
+                min_dist = dist.top().first;
+                break;
+            }
+            dist.pop();
+        }
+        if(min_dist == INF) return INF;
+        ret += min_dist;
+        selected[now] = 1;
+        for(auto edge : adj[now]) dist.push(edge);
+    }
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    cin >> V >> E;
+    while(E--) {
+        int a, b, c; cin >> a >> b >> c;
+        adj[a].push_back({c, b});
+        adj[b].push_back({c, a});
+    }
+    cout << prim(V);
+
+    return 0;
+}
