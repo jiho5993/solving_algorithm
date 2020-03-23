1bca2d2c318769799675c973aa9b31a294451ce6
jiho5993 <jiho5993@naver.com>
2020-03-24 00:53:00 +0900

    BOJ_5618.공약수

--- BOJ/5618.cpp
+++ BOJ/5618.cpp
@@ -1,35 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef priority_queue<int> pqi;
-
-#define fr(i, a, b) for(int i=a; i<b; i++)
 #define pb push_back
 #define mp make_pair
 #define all(x) (x).begin(), (x).end()
-#define pt(a) cout << (a) << '\n'
-#define pt1(a) cout << (a) << ' '
-#define pt2(a, b) cout << (a) << ' ' << (b) << ' '
-
-const int INF = 1e9;
-const int MOD = 1000000007;
-
-int arr[3], n, m=INT32_MAX;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-    
-    cin >> n;
-    fr(i, 0, n) {
-        cin >> arr[i];
-        m = min(m, arr[i]);
-    }
-    fr(i, 1, m)
-        if(arr[0]%i == 0 && arr[1]%i == 0 && arr[2]%i == 0)
-            pt(i);
+  ios_base::sync_with_stdio(false), cin.tie(0);
 
-    return 0;
+  int n, a, b, c=0; cin >> n;
+  if(n == 2) cin >> a >> b;
+  if(n == 3) cin >> a >> b >> c;
+  for(int i=1; i<=min(a, b); i++)
+    if(a%i == 0 && b%i == 0 && c%i == 0)
+      cout << i << '\n';
+
+  return 0;
 }
