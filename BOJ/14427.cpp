f834d3e514544ca3d4a61155c58c1f72f5c3c492
jiho5993 <jiho5993@naver.com>
2020-03-23 19:25:30 +0900

    BOJ_14427.수열과 쿼리 15

--- BOJ/14427.cpp
+++ BOJ/14427.cpp
@@ -1,54 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
 
-ll arr[100001], tree[100001*4];
-
-ll init(int node, int st, int en) {
-    if(st == en) return tree[node] = arr[st];
-    int mid = (st+en)/2;
-    return tree[node] = min(init(node*2, st, mid), init(node*2+1, mid+1, en));
-}
-
-ll update(int node, int st, int en, int idx, ll val) {
-    if(!(st <= idx && idx <= en)) return tree[node];
-    if(st == en) return tree[node] = val;
-    int mid = (st+en)/2;
-    return tree[node] = min(update(node*2, st, mid, idx, val), update(node*2+1, mid+1, en, idx, val));
-}
-
-ll find_min(int node, int st, int en, int left, int right) {
-    if(st > right || en < left) return 2e9;
-    if(left <= st && en <= right) return tree[node];
-    int mid = (st+en)/2;
-    return min(find_min(node*2, st, mid, left, right), find_min(node*2+1, mid+1, en, left, right));
-}
+int N, M;
+int arr[100001];
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0);
+  ios_base::sync_with_stdio(false), cin.tie(0);
 
-    int n, m; cin >> n;
-    for(int i=0; i<n; i++) cin >> arr[i];
-    init(1, 0, n-1);
-    cin >> m;
-    while(m--) {
-        cin.ignore();
-        int query; cin >> query;
-        if(query == 1) {
-            int i, v; cin >> i >> v;
-            arr[i-1] = v;
-            update(1, 0, n-1, i-1, v);
-        } else {
-            ll val = find_min(1, 0, n-1, 0, n-1);
-            for(int i=0; i<n; i++) {
-                if(val == arr[i]) {
-                    cout << i+1 << '\n';
-                    break;
-                }
-            }
-        }
+  cin >> N;
+  set<pair<int, int> > S;
+  for(int i=1; i<=N; i++) {
+    cin >> arr[i];
+    S.insert({arr[i], i});
+  }
+  cin >> M;
+  while(M--) {
+    int q; cin >> q;
+    if(q == 1) {
+      int a, b; cin >> a >> b;
+      S.erase({arr[a], a});
+      arr[a] = b;
+      S.insert({arr[a], a});
+    } else {
+      auto ret = *S.begin();
+      cout << ret.second << '\n';
     }
+  }
 
-    return 0;
+  return 0;
 }
