b55615fef7f306d57ef6f4a3abc6bebde4f082cd
Pjh <jiho5993@naver.com>
2020-05-21 20:56:01 +0900

    leetcode_week4_4.Jump Game

---
+++ 30 day leetcoding challenge/week4/4.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int lastIndex = nums.length - 1;
+        for(int i=lastIndex-1; i>=0; i--) {
+            if(i + nums[i] >= lastIndex) {
+                lastIndex = i;
+            }
+        }
+        return lastIndex == 0;
+    }
+}
