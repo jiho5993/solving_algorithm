084d08798d5a7522525aaa4a612e53d4b55d2db8
Pjh <jiho5993@naver.com>
2020-05-23 23:56:10 +0900

    BOJ_5339.콜센터

---
+++ BOJ/5339.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+  cout << "     /~\\\n";
+  cout << "    ( oo|\n";
+  cout << "    _\\=/_\n";
+  cout << "   /  _  \\\n";
+  cout << "  //|/.\\|\\\\\n";
+  cout << " ||  \\ /  ||\n";
+  cout << "============\n";
+  cout << "|          |\n";
+  cout << "|          |\n";
+  cout << "|          |";
+  
+  return 0;
+}
