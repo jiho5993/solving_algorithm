0fa512f696028aa002696b49a2591d1aa659d9e5
jiho5993 <jiho5993@naver.com>
2020-09-05 16:58:40 +0900

    BOJ_9659.돌 게임 5

---
+++ BOJ/9659.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0);
+
+    ll N; cin >> N;
+    N&1 ? cout << "SK" : cout << "CY";
+
+    return 0;
+}
