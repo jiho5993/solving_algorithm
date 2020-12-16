064d43b822a085b02bbc5ac2ea36ee5b1958ea95
박지호 <jiho5993@naver.com>
2020-12-16 23:55:36 +0900

    BOJ_18108.1998년생인 내가 태국에서는 2541년생?

---
+++ BOJ/18108.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define V(T) vector<T>
+#define VP(T) vector<pair<T, T> >
+#define P(T) pair<T, T>
+#define PQ(T) priority_queue<T>
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0);
+
+    int N; cin >> N;
+    cout << N-543;
+
+    return 0;
+}
