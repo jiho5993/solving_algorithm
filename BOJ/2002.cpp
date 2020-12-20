da3339693d9f75a045e23db68b87e9f783127e53
박지호 <jiho5993@naver.com>
2020-12-20 21:23:49 +0900

    BOJ_2002.추월

---
+++ BOJ/2002.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0)
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+#define sc(x) cin>>x;
+#define sc2(x,y) cin>>x>>y
+#define sc3(x,y,z) cin>>x>>y>>z
+#define sc4(x,y,z,w) cin>>x>>y>>z>>w
+#define P(T) pair<T,T>
+#define V(T) vector<T>
+#define Q(T) queue<T,T>
+#define S(T) stack<T>
+#define DQ(T) deque<T>
+#define VP(T) vector<pair<T,T>>
+#define QP(T) queue<pair<T,T>>
+#define SP(T) stack<pair<T,T>>
+#define PQ(T) priority_queue<T>
+#define GPQ(T) priority_queue<T,vector<T>,greater<T>>
+
+int main() {
+    FASTIO;
+
+    int N, cnt=0; sc(N);
+    map<string, int> in;
+    int out[N+1];
+    for(int i=0; i<N; i++) {
+        string s; sc(s);
+        in[s] = i;
+    }
+    for(int i=0; i<N; i++) {
+        string s; sc(s);
+        out[i] = in[s];
+    }
+    for(int i=0; i<N-1; i++) {
+        for(int j=i+1; j<N; j++) {
+            if(out[i] > out[j]) {
+                cnt++;
+                break;
+            }
+        }
+    }
+    cout << cnt;
+
+    return 0;
+}
