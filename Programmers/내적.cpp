11fc723e5180341d5c40b488c1848b3694b32499
박지호 <jiho5993@naver.com>
2020-12-22 16:33:31 +0900

    Programmers.내적

---
+++ Programmers/내적.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0)
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define ll long long
+#define sc(x) cin>>x;
+#define sc2(x,y) cin>>x>>y
+#define sc3(x,y,z) cin>>x>>y>>z
+#define sc4(x,y,z,w) cin>>x>>y>>z>>w
+#define P(T) pair<T,T>
+#define V(T) vector<T>
+#define Q(T) queue<T,T>
+#define S(T) stack<T>
+#define DQ(T) deque<T>
+#define VP(T) vector<pair<T,T>>
+#define QP(T) queue<pair<T,T>>
+#define SP(T) stack<pair<T,T>>
+#define PQ(T) priority_queue<T>
+#define GPQ(T) priority_queue<T,vector<T>,greater<T>>
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+
+    for(int i=0; i<a.size(); i++) answer += a[i]*b[i];
+
+    return answer;
+}
+
+int main() {
+    FASTIO;
+
+    int res = solution({-1, 0, 1}, {1, 0, -1});
+    cout << res;
+
+    return 0;
+}
