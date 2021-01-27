95fe74f965205e5288c5532f2ee58393aafe360b
박지호 <jiho5993@naver.com>
2021-01-27 21:49:55 +0900

    BOJ_11441.합 구하기

---
+++ BOJ/11441.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+int N, M;
+int arr[100001];
+
+int main() {
+    ios_base::sync_with_stdio(false),cin.tie(0);
+
+    cin >> N;
+    for(int i=0; i<N; i++) cin >> arr[i];
+    for(int i=1; i<N; i++) arr[i] += arr[i-1];
+    cin >> M;
+    while(M--) {
+        int a, b; cin >> a >> b;
+        cout << arr[b-1]-arr[a-2] << '\n';
+    }
+
+    return 0;
+}
