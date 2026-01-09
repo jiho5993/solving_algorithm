afb5b1d0e5e3f97f60ec31683ed2d982e9ce0947
박지호 <jiho5993@gmail.com>
2026-01-09 05:56:49 +0000

    BOJ_34945_26_01_09

---
+++ BOJ/34945_26_01_09.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL_TEST
+constexpr bool isLOCAL = true;
+#else
+constexpr bool isLOCAL = false;
+#endif
+
+#define debug if constexpr (isLOCAL) cerr
+#define ll long long
+#define pii pair<int, int>¸
+
+void solve() {
+    int a; cin >> a;
+    if (a >= 6) cout << "Success!\n";
+    else cout << "Oh My God!\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    solve();
+
+    debug << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+
+    return 0;
+}
